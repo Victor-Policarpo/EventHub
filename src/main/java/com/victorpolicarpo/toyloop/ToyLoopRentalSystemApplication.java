3318b433f2443a22347eb534ea2452228bcdd54f
Victor-Policarpo <victorpolicarposilva@gmail.com>
2026-03-03 12:05:35 -0300

    chore: initial setup with Spring configuration and Docker environment

---
+++ src/main/java/com/victorpolicarpo/toyloop/ToyLoopRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.victorpolicarpo.toyloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyLoopRentalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyLoopRentalSystemApplication.class, args);
+	}
+
+}
