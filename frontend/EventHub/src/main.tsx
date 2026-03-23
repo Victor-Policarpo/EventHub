31ac21c8f70ad37f16af0ca101e8b25aabcc391f
Victor-Policarpo <victorpolicarposilva@gmail.com>
2026-03-23 15:38:24 -0300

    feat: inital project setup with React, TypeScript and Tailwind CSS

--- frontend/EventHub/src/main.tsx
+++ frontend/EventHub/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Login from './pages/index'
+import Login from './pages/Login/Index'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Login />
   </StrictMode>,
